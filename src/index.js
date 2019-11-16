6b273bed9f4c1fe2fbfe0eb1f7c84f1cd7b9fb14
Sean Payne <sean.d.payne@gmail.com>
2019-11-15 18:18:58 -0800

    add axis labels

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 var THREE=require('three');
 
-import { CSS3DRenderer } from 'three/examples/jsm/renderers/CSS3DRenderer.js'
+import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer.js'
 
 
-var scene = new THREE.Scene();
+var sceneGL = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, 
 window.innerWidth / window.innerHeight, 
 0.1, 1000 );
 
+var sceneCSS = new THREE.Scene();
 var rendererCSS = new CSS3DRenderer();
 rendererCSS.setSize( window.innerWidth, window.innerHeight );
 rendererCSS.domElement.style.position = 'absolute';
 rendererCSS.domElement.style.top = 0;
-document.getElementById( 'container' ).appendChild( rendererCSS.domElement );
 
 var rendererGL = new THREE.WebGLRenderer({ antialias: true, alpha:true });
 rendererGL.setSize( window.innerWidth, window.innerHeight );
 rendererGL.domElement.style.position = 'absolute';
-rendererGL.domElement.style.zIndex = 1;
+rendererGL.domElement.style.zIndex = -1;
 rendererGL.domElement.style.top = 0;
-document.getElementById( 'container' ).appendChild( rendererGL.domElement );
+
+// ref: https://stackoverflow.com/questions/24681170/three-js-properly-blending-css3d-and-webgl
+//rendererCSS.domElement.appendChild( rendererGL.domElement );
+var container = document.getElementById( 'container' );
+container.appendChild( rendererGL.domElement );
+container.appendChild( rendererCSS.domElement );
 
 
 var geometry = new THREE.RingBufferGeometry( 0.9, 1, 62 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+sceneGL.add( cube );
 
 
 var grid_size = 4;
@@ -37,9 +42,33 @@ var gridHelper = new THREE.GridHelper(
     grid_color_center_line,
     grid_color);
 
-scene.add( gridHelper );
+sceneGL.add( gridHelper );
 gridHelper.rotateX(Math.PI / 2);
 
+
+// CSS Objects
+for (var i = 0; i < 8; i++) {
+
+    var yoffset = -0.2;
+    var xoffset = -0.2;
+    var xpos = [-2, -1, 1, 2, xoffset, xoffset, xoffset, xoffset]
+    var ypos = [yoffset, yoffset, yoffset, yoffset,-2, -1, 1, 2];
+    var zpos = [0, 0, 0, 0, 0, 0, 0, 0];
+    var text = ["-2", "-1" ,"1","2", "-2i", "-i", "i", "2i"];
+
+    var element = document.createElement('div');
+    element.setAttribute("class", "AxisLabel");
+    var content = document.createTextNode(text[i]);
+    element.appendChild(content);
+    
+    var object = new CSS3DObject(element);
+    object.position.x = xpos[i];
+    object.position.y = ypos[i];
+    object.position.z = zpos[i];
+    object.scale.x = 0.1;
+    object.scale.y = 0.1;
+    sceneCSS.add(object);
+}
 
 
 
@@ -51,14 +80,15 @@ button.addEventListener( 'click', function () {
 }, false );
 
 
+
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame( animate );
-    cube.rotateX(rotx);
-    rendererCSS.render( scene, camera );
-    rendererGL.render( scene, camera );
+    //cube.rotateX(rotx);
     
+    rendererGL.render( sceneGL, camera );
+    rendererCSS.render( sceneCSS, camera );
 }
 animate();
 
